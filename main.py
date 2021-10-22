289ca344627ff749a3cc0f81f87ecf9e77311c27
Chuying.He@ibm.com <Chuying.He@ibm.com>
2021-10-22 09:53:13 +0200

    create python project

---
+++ main.py
@@ -0,0 +1,9 @@
+# Program to display the Fibonacci sequence up to n-th term
+def printString(name, program):
+    return f'Hello {name}! This is {program}'
+
+def runCode():
+    print(printString('world', 'python'))
+
+if __name__ == "__main__":
+    runCode()
