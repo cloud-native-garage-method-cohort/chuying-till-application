a93e1072e90b5fa97702ff0a9d853e84c42a56b7
tbrodbeck@uos.de <tbrodbeck@uos.de>
2021-10-22 12:02:53 +0200

    renamed function

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Program to display the Fibonacci sequence up to n-th term
-def printString(name, program):
+def greet(name, program):
     return f'Hello {name}! This is {program}'
 
 def runCode():
-    print(printString('world', 'python'))
+    print(greet('world', 'python'))
 
 if __name__ == "__main__":
     runCode()
